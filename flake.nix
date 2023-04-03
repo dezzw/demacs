d71226f290e5d5f2633f4cf56531c1bd27cb4d20
dezzw <dw@dezzw.com>
2023-04-03 02:41:04 -0400

    Setup emacs build with webkit and treesit

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  description = "Personal custom Emacs builds for macOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    emacs-src = {
+      url = "github:emacs-mirror/emacs/emacs-29";
+      flake = false;
+    };
+
+    emacs-vterm-src = {
+      url = "github:akermu/emacs-libvterm";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, emacs-src, emacs-vterm-src }:
+    let
+      supportedSystems = [ "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      packages = forAllSystems (system: {
+        emacs = nixpkgsFor.${system}.emacs;
+        emacs-vterm = nixpkgsFor.${system}.emacs-vterm;
+      });
+
+      overlay = final: prev: {
+        emacs-vterm = prev.stdenv.mkDerivation rec {
+          pname = "emacs-vterm";
+          version = "master";
+
+          src = emacs-vterm-src;
+
+          nativeBuildInputs = [
+            prev.cmake
+            prev.libtool
+            prev.glib.dev
+          ];
+
+          buildInputs = [
+            prev.glib.out
+            prev.libvterm-neovim
+            prev.ncurses
+          ];
+
+          cmakeFlags = [
+            "-DUSE_SYSTEM_LIBVTERM=yes"
+          ];
+
+          preConfigure = ''
+            echo "include_directories(\"${prev.glib.out}/lib/glib-2.0/include\")" >> CMakeLists.txt
+            echo "include_directories(\"${prev.glib.dev}/include/glib-2.0\")" >> CMakeLists.txt
+            echo "include_directories(\"${prev.ncurses.dev}/include\")" >> CMakeLists.txt
+            echo "include_directories(\"${prev.libvterm-neovim}/include\")" >> CMakeLists.txt
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp ../vterm-module.so $out
+            cp ../vterm.el $out
+          '';
+        };
+
+        emacs = (prev.emacs.override { srcRepo = true; nativeComp = true; withXwidgets = true; withGTK3 = true; withSQLite3 = true; withWebP = true; }).overrideAttrs (
+          o: rec {
+            version = "29.0.60";
+            src = emacs-src;
+
+            buildInputs = o.buildInputs ++ [ prev.pkgs.tree-sitter prev.darwin.apple_sdk.frameworks.WebKit ] ;
+
+            patches = [
+              ./patches/fix-window-role.patch
+              ./patches/poll.patch
+              ./patches/round-undecorated-frame.patch
+              ./patches/system-appearance.patch
+            ];
+
+            postPatch = o.postPatch + ''
+              substituteInPlace lisp/loadup.el \
+              --replace '(emacs-repository-get-branch)' '"master"'
+            '';
+
+            postInstall = o.postInstall + ''
+              cp ${final.emacs-vterm}/vterm.el $out/share/emacs/site-lisp/vterm.el
+              cp ${final.emacs-vterm}/vterm-module.so $out/share/emacs/site-lisp/vterm-module.so
+            '';
+
+            CFLAGS = "-DMAC_OS_X_VERSION_MAX_ALLOWED=110203 -g -O2";
+          }
+        );
+      };
+    };
+}
