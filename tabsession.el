e4556a97583d5e41b694f4d9e07bbc16e7c7a19a
dezzw <dw@dezzw.com>
2023-11-07 10:20:41 -0500

    remove unused files

--- tabsession.el
+++
@@ -1,8 +0,0 @@
-;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
-;; -------------------------------------------------------------------------
-;; Tabspaces Session File for Emacs
-;; -------------------------------------------------------------------------
-;; Created Mon Nov  6 00:54:55 2023
-
-;; Tabs and buffers:
-(setq tabspaces--session-list '((("/Users/dez/demacs/early-init.el" "/Users/dez/.dotfiles/modules/darwin/core.nix") . "*scratch*") (("/Users/dez/demacs/flake.nix" "/Users/dez/.dotfiles/modules/emacs/default.nix" "/Users/dez/.dotfiles/modules/darwin/core.nix" "/Users/dez/.dotfiles/modules/darwin/brew.nix") . "384A3")))
